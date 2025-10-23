a8338a4f75b9655693dc1f63679f935f0ecee92e
Guimirand4 <guilhermemiranda419@gmail.com>
2025-10-22 21:44:43 -0300

    Corrige variável de ambiente para Create React App

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL || process.env.REACT_APP_BACKEND_URL || "https://movies-list-nine-psi.vercel.app",
+  baseURL: process.env.REACT_APP_BACKEND_URL || "https://movies-list-nine-psi.vercel.app",
 });
 
 export default api;
