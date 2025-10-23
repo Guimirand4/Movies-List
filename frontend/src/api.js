103d56c784f6fd28056fe976549e776ca804ead4
Guimirand4 <guilhermemiranda419@gmail.com>
2025-10-22 21:38:14 -0300

    feat: base de dados para vercel

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5050", // sua porta do backend!
+  baseURL: import.meta.env.VITE_BACKEND_URL || process.env.REACT_APP_BACKEND_URL || "https://movies-list-nine-psi.vercel.app",
 });
 
 export default api;
